c889accbb282da235820b7295922c6e31376824c
Ricardo Souza <ricksouz96@gmail.com>
2023-10-23 20:44:14 -0300

    Aplly the solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const len = word.length;
+    let reversed = '';
+    for(let i = len; i >= 0; i--) {
+        let char = word.charAt(i);
+        reversed += char;
+    }
+    return reversed;
 };
+
+const word = 'hello';
+reverseString(word);
 
 // Do not edit below this line
 module.exports = reverseString;
