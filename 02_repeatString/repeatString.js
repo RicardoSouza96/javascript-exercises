5a64616124820df297fe2b1fd37b5e203ea0f533
Ricardo Souza <ricksouz96@gmail.com>
2023-10-23 20:30:22 -0300

    Aplly the solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(frase, repetitions) {
+    if(repetitions < 0)
+        return 'ERROR';
+    let result = '';
+    for(let i = 0; i < repetitions; i++) {
+        result += frase;
+    }
+    return result;
 };
+
+const frase = 'hey';
+const repetitions = 3;
+repeatString(frase, repetitions);
 
 // Do not edit below this line
 module.exports = repeatString;
