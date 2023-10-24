2e3bb785ec1391a0c5b5375eefe15b4d1d424200
Ricardo Souza <ricksouz96@gmail.com>
2023-10-24 20:21:16 -0300

    Aplly the solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(val1, val2) {
+    let sum = 0;
+    if(!Number.isInteger(val1) || !Number.isInteger(val2))
+        return "ERROR";
+    if(val1 < 0 || val2 < 0)
+        return "ERROR";
+    if(val1 < val2) {
+        for(; val1 <= val2; val1++) {
+            sum += val1;
+        }
+        return sum;
+    }
+    if(val2 < val1) {
+        for(; val2 <= val1; val2++) {
+            sum += val2;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
